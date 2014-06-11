464aac9b93e2a47ab5ea2ef07a2022a98f705018
Manuel Aristarán <jazzido@jazzido.com>
2014-06-11 00:37:30 -0500

    line draggin'

--- public/app.js
+++ public/app.js
@@ -14,7 +14,7 @@ var TableView = Backbone.View.extend({
     opacity: 0
 
   },
-  tool: null,
+  defaultTool: null,
   lastHitRectangle: null,
   paperScope: {},
   cells: null,
@@ -24,19 +24,18 @@ var TableView = Backbone.View.extend({
     this.bounds = options.bounds;
     paper.install(this.paperScope);
     this.render();
+    this._createDefaultTool();
   },
 
   render: function() {
     paper.setup(this.el);
-    this.tool = new this.paperScope.Tool();
-    this._installHandlers();
     var that = this;
     $.get('/cells.json', function(data) {
       _.each(data.cells, function(cell) {
         var r = new that.paperScope.Path.Rectangle(cell[1], cell[0], cell[2], cell[3]);
         r.data = 'cell';
         r.style = that.rectangleStyle;
-       });
+      });
       _.each(data.vertical_rulings, function(r) {
         var line = new that.paperScope.Path.Line(new that.paperScope.Point(r[0], r[1]), new that.paperScope.Point(r[2], r[3]));
         line.data = 'vertical';
@@ -51,63 +50,63 @@ var TableView = Backbone.View.extend({
     });
   },
 
-  _installHandlers: function() {
+  _createDefaultTool: function() {
+    this.defaultTool = new this.paperScope.Tool();
+    this.defaultTool.onMouseDrag = _.bind(function(event) {
+                         if (this.draggedElement === null) {
+                           return;
+                         }
+                         switch (this.draggedElement.data) {
+                           case 'vertical':
+                           this.draggedElement.position = new this.paperScope.Point(event.point.x, this.draggedElement.position.y);
+                           break;
+                           case 'horizontal':
+                           this.draggedElement.position = new this.paperScope.Point(this.draggedElement.position.x, event.point.y);
+                           break;
+                         }
+                       }, this);
 
-    this.tool.onMouseDrag = _.bind(function(event) {
-                              if (this.draggedElement === null) {
-                                return;
-                              }
-                              switch (this.draggedElement.data) {
-                                case 'vertical':
-                                this.draggedElement.position = new this.paperScope.Point(event.point.x, this.draggedElement.position.y);
-                                break;
-                                case 'horizontal':
-                                this.draggedElement.position = new this.paperScope.Point(this.draggedElement.position.x, event.point.y);
-                                break;
-                              }
-                            }, this);
+    this.defaultTool.onMouseUp = _.bind(function() {
+                       this.draggedElement = null;
+                     }, this);
 
-    this.tool.onMouseUp = _.bind(function(event) {
-                            this.draggedElement = null;
-                          }, this);
-
-    this.tool.onMouseDown = _.bind(function(event) {
-                              var hitResult = this.paperScope.project.hitTest(event.point, this.hitOptions);
-                              if (!hitResult && hitResult.item.data !== 'horizontal' && hitResult.item.data !== 'vertical') {
-                                this.draggedElement = null;
-                                return;
-                              }
+    this.defaultTool.onMouseDown = _.bind(function(event) {
+                         var hitResult = this.paperScope.project.hitTest(event.point, this.hitOptions);
+                         if (!hitResult && hitResult.item.data !== 'horizontal' && hitResult.item.data !== 'vertical') {
+                           this.draggedElement = null;
+                           return;
+                         }
 
 
-                              this.draggedElement = hitResult.item;
-                          }, this);
+                         this.draggedElement = hitResult.item;
+                       }, this);
 
-    this.tool.onMouseMove = _.bind(function(event) {
-                              var hitResult = this.paperScope.project.hitTest(event.point, this.hitOptions);
-                              if (!hitResult) {
-                                $(this.paperScope.view.element).removeClass('horizontalMove verticalMove');
-                                return;
-                              }
+    this.defaultTool.onMouseMove = _.bind(function(event) {
+                         var hitResult = this.paperScope.project.hitTest(event.point, this.hitOptions);
+                         if (!hitResult) {
+                           $(this.paperScope.view.element).removeClass('horizontalMove verticalMove');
+                           return;
+                         }
 
-                              switch(hitResult.item.data) {
-                                case 'cell':
-                                if (this.lastHitRectangle === hitResult.item) return;
-                                $(this.paperScope.view.element).removeClass('horizontalMove verticalMove');
-                                if (this.lastHitRectangle !== null) {
-                                  this.lastHitRectangle.fillColor = 'white';
-                                }
-                                hitResult.item.fillColor = 'blue';
-                                this.lastHitRectangle = hitResult.item;
-                                break;
-                                case 'horizontal':
-                                $(this.paperScope.view.element).addClass('horizontalMove');
-                                break;
-                                case 'vertical':
-                                $(this.paperScope.view.element).addClass('verticalMove');
-                                break;
-                              }
-                            }, this);
-  }
+                         switch(hitResult.item.data) {
+                           case 'cell':
+                           if (this.lastHitRectangle === hitResult.item) return;
+                           $(this.paperScope.view.element).removeClass('horizontalMove verticalMove');
+                           if (this.lastHitRectangle !== null) {
+                             this.lastHitRectangle.fillColor = 'white';
+                           }
+                           hitResult.item.fillColor = 'blue';
+                           this.lastHitRectangle = hitResult.item;
+                           break;
+                           case 'horizontal':
+                           $(this.paperScope.view.element).addClass('horizontalMove');
+                           break;
+                           case 'vertical':
+                           $(this.paperScope.view.element).addClass('verticalMove');
+                           break;
+                         }
+                       }, this);
+  },
 });
 
 $(function() {
